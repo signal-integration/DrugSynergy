850b5957c2db4725a48ce6c9810b86df68a71a9a
Antonio <antonio.cappuccio@curie.fr>
2017-10-16 16:05:12 +0200

    updating filter on delta

--- codes/filter_data_on_deltas.R
+++ codes/filter_data_on_deltas.R
@@ -1,4 +1,9 @@
 filter_data_on_deltas = function(my_data, design, min_delta = 0.5){
+  #removes from a X+Y dataset all genes (rows) whose average non-zero deltas are all small than
+  #minimum delta. This is the analogous of a two fold-change when analyzing two conditions.
+  
+  #Inputs: an X+Y dataset; design: the experimental design; min_delta: minimum non-zero delta imposed
+  #Output: a filtered X+Y dataset
   
   source("compute_minimum_delta_analysis.R")
   
