53f302d24d419b5eb1b383854f662045e0bef1fa
Antonio <antonio.cappuccio@curie.fr>
2017-12-21 11:50:36 +0100

    updating compute delta and means

--- codes/compute_minimum_delta.R
+++ codes/compute_minimum_delta.R
@@ -1,17 +1,37 @@
-compute_minimum_delta = function(Mu,PROFCODES,prof_index){
+compute_minimum_delta = function(profile_means,PROFCODES,prof_index){
 
-  #this function computes the smallest non-zero difference between the mean values of a given profile
-  #INPUTS 
-  #Mu: vector of mean values 
-  #PROFCODES: definition of profiles
-  #prof_index: profile index
+  #REQUIRES
+  #profile_means: a vector of profile means (e0, eX, eY, eX+Y) as computed by "compute_profile_means" 
+  #PROFCODES: definitions of all profiles in terms of ternary vectors
+  #prof_index = index of a profile of interest (1 to 123)
 
-  ADD = Mu[1] + Mu[2] - Mu[1] + Mu[3] - Mu[1]
+  #OUTPUT
+  #the minimum non-zero difference of all pairwise comparisons among (additive_level, e0, eX, eY, eX+Y) 
+
+  e_0 = profile_means[1]
+  e_X = profile_means[2]
+  e_Y = profile_means[3]
+  e_XY = profile_means[4]
   
-  V1 = c(ADD - Mu[1],Mu[2] - Mu[1],Mu[3] - Mu[1],Mu[4] - Mu[1],
-         Mu[2] - ADD,Mu[3] - ADD,Mu[4] - ADD,Mu[3] - Mu[2],
-         Mu[4] - Mu[2],Mu[4] - Mu[3])
+  additive_level = e_0 + (e_X - e_0) + (e_Y - e_0)
+  
+  pairwise_deltas = c(additive_level - e_0,
+                      e_X - e_0,
+                      e_Y - e_0,
+                      e_XY - e_0,
+                      e_X - additive_level,
+                      e_Y - additive_level,
+                      e_XY - additive_level,
+                      e_Y - e_X,
+                      e_XY - e_X,
+                      e_XY - e_Y)
 
-  return(min(abs(V1[ which(PROFCODES[prof_index,1:10]!=0)] )))
+  non_zero_pairwise_deltas_indeces = which(PROFCODES[prof_index,1:10]!=0)
+  
+  non_zero_pairwise_deltas = pairwise_deltas[non_zero_pairwise_deltas_indeces]
+  
+  min_delta = min(abs(non_zero_pairwise_deltas))
+  
+  return(min_delta)
   
   }
--- codes/compute_profile_means.R
+++ codes/compute_profile_means.R
@@ -6,22 +6,22 @@ compute_profile_means = function(PROFCODES,
                                  constraints_vector,
                                  min_delta) {
   
-  #this function computes a vector of mean values of (e0, eX, eY, eX+Y) for a profile of interest.
+  #computes a vector of mean values of (e0, eX, eY, eX+Y) for a profile of interest.
   
-  #INPUTS
-  #PROFCODES (definitions of all profiles) 
-  #prof_index: index of profile of interest
-  #ntimes: how many different vectors of means is computed;
-  #exp_min: min of expression range 
+  #REQUIRES
+  #PROFCODES (definitions of all profiles in terms of ternary vectors) 
+  #prof_index: index of profile of interest;
+  #ntimes: how many different vectors of means are computed;
+  #exp_min: min of expression range;
   #exp_max: max of expression range;
   #constraints_vector: expresses the inequalities of the profiles in the format needed for linsolve
-  #min_delta: minimum non-zero difference of all pairwise comparisons
+  #min_delta: minimum signal (minimum non-zero difference of all pairwise comparisons, in absolute value)
   
   #OUTPUT
-  #a vector of mean values of (e0, eX, eY, eX+Y) for a profile of interest
+  #a vector of mean values of (e0, eX, eY, eX+Y) simulating a profile of interest
   
   source("compute_minimum_delta.R")
-  
+
   library(limSolve)
   
   options(warn = -1)
@@ -33,24 +33,30 @@ compute_profile_means = function(PROFCODES,
   F = cbind(rep(0, dim(E)[1]))
   
   G1 = matrix(constraints_vector[b == 1, ], ncol = 4)
+  
   H1 = cbind(rep(0, dim(G1)[1]))
   
   G2 = -matrix(constraints_vector[b == -1, ], ncol = 4)
+  
   H2 = cbind(rep(0, dim(G2)[1]))
   
   Gvar = rbind(diag(4), -diag(4))
   
   Hvar = cbind(c(
+    
     exp_min,
     exp_min,
     exp_min,
-    exp_min,-exp_max,
+    exp_min,
+    -exp_max,
     -exp_max,
     -exp_max,
     -exp_max
+    
   ))
   
   G = rbind(G1, G2, Gvar)
+  
   H = rbind(H1, H2, Hvar)
   
   if (dim(E)[1] == 0 & dim(G)[1] > 0) {
@@ -59,14 +65,9 @@ compute_profile_means = function(PROFCODES,
                     iter = (5 * ntimes + 1))[[1]]
     
   } else {
-    synth = xsample(
-      E = E,
-      F = F,
-      G = G,
-      H = H,
-      iter = (5 * ntimes + 1)
-    )[[1]]
     
+    synth = xsample(E = E, F = F, G = G,
+      H = H, iter = (5 * ntimes + 1))[[1]]
   }
   
   synth = synth[-1, ]
