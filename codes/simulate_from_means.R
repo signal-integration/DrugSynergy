724c062166923bb7184a6ed4d3b9000202df9b69
Antonio <antonio.cappuccio@curie.fr>
2017-12-21 12:10:09 +0100

    improved simulate from means

--- codes/simulate_from_means.R
+++ codes/simulate_from_means.R
@@ -1,24 +1,37 @@
-simulate_from_means=function(profile_means, prof_code, replicates, noise, expr_min, expr_max){
+simulate_from_means = function(profile_means, replicates, noise, exp_min, exp_max){
   
-  Mu = profile_means
-
-  # ADD = Mu[1] + Mu[2] - Mu[1] + Mu[3] - Mu[1]
-  # 
-  # V1 = c(ADD - Mu[1], Mu[2] - Mu[1], Mu[3] - Mu[1], Mu[4] - Mu[1],
-  #        Mu[2] - ADD, Mu[3] - ADD, Mu[4] - ADD, Mu[3] - Mu[2],
-  #        Mu[4] - Mu[2], Mu[4] - Mu[3])
+  #REQUIRES
+  #profile_means: a vector of profile means (e0, eX, eY, eX+Y) as computed by "compute_profile_means" 
+  #replicates = how many replicates should be simulated for each condition
+  #noise: sigma in the normal distribution
+  #exp_min = minimum value of the expression range
+  #exp_max = maximum value of the expression range
   
-  expr = c(
+  #OUTPUT
+  #a simulated profile with replicated instances of e0, eX, eY, eX+Y
+  
+  e_0 = profile_means[1]
+  e_X = profile_means[2]
+  e_Y = profile_means[3]
+  e_XY = profile_means[4]
+  
+  simulated_profile = c(
     
-    rnorm(replicates, Mu[1], sd = noise), rnorm(replicates, Mu[2], sd = noise),
-    
-    rnorm(replicates, Mu[3], sd = noise), rnorm(replicates, Mu[4], sd = noise)
-  )
+    rnorm(replicates, e_0, sd = noise), 
+    rnorm(replicates, e_X, sd = noise),
+    rnorm(replicates, e_Y, sd = noise), 
+    rnorm(replicates, e_XY, sd = noise)
   
-  #keep expression values within expression range
-  expr[expr<expr_min]=expr_min
-  expr[expr>expr_max]=expr_max
+    )
   
-  return(expr)
+  #keep simulated_profileession values within simulated_profileession range
+  simulated_profile[simulated_profile < exp_min] = exp_min
+  simulated_profile[simulated_profile > exp_max] = exp_max
+  
+  names(simulated_profile) = c(rep("0", replicates),
+                               rep("X", replicates),
+                               rep("Y", replicates),
+                               rep("Y+X", replicates))
+  return(simulated_profile)
   
 }
