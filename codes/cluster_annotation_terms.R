8a6755cf60db4f582342f26b3715a4fea21275e4
Antonio <antonio.cappuccio@curie.fr>
2018-02-20 22:29:35 +0100

    b

---
+++ codes/cluster_annotation_terms.R
@@ -0,0 +1,44 @@
+cluster_annotation_terms = function(deg, inter_type, dbs, n = 20){
+  
+  library(enrichR)
+  library(ggplot2)
+  
+  joined = data.frame()
+  gene_set = deg[deg$type == inter_type, 'genes']
+  enriched <- do.call("rbind", enrichr(gene_set, dbs))
+  enriched = enriched[order(enriched$P.value)[1:n], 
+                      c("Term", "Overlap", "P.value")]
+  enriched$size = length(gene_set)
+  enriched$gene_set = 'all'
+  joined = rbind(joined, enriched)
+  
+  for (h in c(1, 4, 5)){
+    
+    gene_set = deg[deg$type == inter_type & deg$case == h, 'genes']
+    
+    enriched <- enrichr(gene_set, dbs)[[1]]
+    enriched = enriched[order(enriched$P.value)[1:n], 
+                        c("Term", "Overlap", "P.value")]
+    enriched$size = length(gene_set)
+    enriched$gene_set = as.character(h)
+    joined = rbind(joined, enriched)
+    
+  }
+  
+  joined$Term = strtrim(joined$Term, 40)
+  
+  
+  #manipulate dataframe
+  joined$score = -log10(joined$P.value)
+  joined$hits = sapply(strsplit(joined$Overlap, '/'), function(x) as.numeric(x[[1]]))
+  joined$ratio = joined$hits/joined$size
+  
+  joined = na.omit(joined)
+  joined = joined[joined$hits >=2, ]
+  joined = joined[joined$P.value <=0.05, ]
+  
+  joined$gene_set = relevel(factor(joined$gene_set), "all")
+
+  return(joined)  
+  
+  }
--- codes/generate_ocean_plot.R
+++ codes/generate_ocean_plot.R
@@ -9,11 +9,15 @@ generate_ocean_plot = function(deg){
   
   alluvial_cases = melt(table(deg$type, deg$case))
   
-  col = as.character(alluvial_cases$Var.1)
+  col = as.character(alluvial_cases$Var1)
   col[col == 'N'] = 'red'
   col[col == 'P'] = 'blue'
   
+  alluvial_cases$Var2 = factor(alluvial_cases$Var2, 
+                               levels = rev(unique(alluvial_cases$Var2)))
+  
   alluvial(alluvial_cases[,1:2], freq = alluvial_cases$value, 
            col = col, cex = 0.01, alpha=0.8, 
-           hide = alluvial_cases$value < 10)
+           hide = alluvial_cases$value < 10,
+           layer = alluvial_cases$Var1 == 'N')
   }
